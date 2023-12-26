8cf4701a890d7ea6097348c45f4840996023ae7c
krisciu <kriscija@gmail.com>
2023-12-25 20:03:18 -0500

    update

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createSnowflake() {
 setInterval(createSnowflake, 100);
 
 function createLight() {
-    const lightColors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff']; // Array of light colors
+    const lightColors = ['#ff0000', '#00ff00']; // Array of light colors
     const lightColor = lightColors[Math.floor(Math.random() * lightColors.length)]; // Randomly pick one
 
     const light = document.createElement('div');
@@ -36,18 +36,41 @@ function createLight() {
 
 setInterval(createLight, 150);
 
-function assignRandomPath(element) {
-    const paths = ['flyPath1', 'flyPath2', 'flyPath3']; // List your paths here
-    const randomPath = paths[Math.floor(Math.random() * paths.length)];
-    element.style.animationName = randomPath;
+function moveCharacter(character, speed) {
+    let x = Math.random() * (window.innerWidth - character.offsetWidth);
+    let y = Math.random() * (window.innerHeight - character.offsetHeight);
+    let deltaX = (Math.random() < 0.5 ? -1 : 1) * speed;
+    let deltaY = (Math.random() < 0.5 ? -1 : 1) * speed;
+
+    function updatePosition() {
+        if (x <= 0 || x >= window.innerWidth - character.offsetWidth) {
+            character.remove();
+            setTimeout(() => spawnCharacter(character), 2000); // Respawn after 2 seconds
+            return;
+        }
+        if (y <= 0 || y >= window.innerHeight - character.offsetHeight) {
+            character.remove();
+            setTimeout(() => spawnCharacter(character), 2000); // Respawn after 2 seconds
+            return;
+        }
+        x += deltaX;
+        y += deltaY;
+        character.style.left = x + 'px';
+        character.style.top = y + 'px';
+
+        requestAnimationFrame(updatePosition);
+    }
+
+    updatePosition();
 }
 
-// For Santa and Jesus images
+function spawnCharacter(character) {
+    document.body.appendChild(character);
+    moveCharacter(character, 2); // Speed can be adjusted
+}
+
 const santa = document.getElementById('santa');
 const jesus = document.getElementById('jesus');
-
-setInterval(() => {
-    assignRandomPath(santa);
-    assignRandomPath(jesus);
-}, 10000); // Adjust time as needed
+spawnCharacter(santa);
+spawnCharacter(jesus);
 
