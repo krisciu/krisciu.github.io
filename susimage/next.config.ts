00ffb237638427a26fe41fe72b72adbd77639453
Joe Krisciunas <94765236+krisciu@users.noreply.github.com>
2024-12-21 03:17:51 -0800

    remove dir from gh-pages

--- susimage/next.config.ts
+++ susimage/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Since this will be hosted at krisciu.github.io/susimage
-  basePath: '/susimage',
-  assetPrefix: '/susimage/',
 }
 
 module.exports = nextConfig
